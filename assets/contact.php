eb303c757df4d06a34a9b88b8a7517a2e626ea8f
Charlie Thomas <93368865+charliethomasct82@users.noreply.github.com>
2024-05-19 18:42:13 +0100

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -19,7 +19,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host       = 'smtp.gmail.com';
         $mail->SMTPAuth   = true;
         $mail->Username   = 'charliethomasctg@gmail.com'; // Your Gmail address
-        $mail->Password   = 'Matthias@2012';    // Your Gmail App Password
+        $mail->Password   = '#######';    // Your Gmail App Password
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port       = 587;
 
